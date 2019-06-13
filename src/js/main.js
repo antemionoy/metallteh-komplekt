e56de1eadd4d546058c5d23ac66836467939ea5b
Anton <antemionoy@gmail.com>
2019-06-13 18:41:01 +0300

    select and inputs range styled

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,109 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/owl.carousel/dist/owl.carousel.js
+//= ../../bower_components/nouislider/distribute/nouislider.min.js
+//= ../../bower_components/remodal/dist/remodal.min.js
+//= ../../bower_components/wnumb/wnumb.js
 
 "use strict";
+
+function selectFun() {
+
+    $('.form__select').each(function() {
+        var $this = $(this),
+            numberOfOptions = $(this).children('option').length;
+
+        $this.addClass('select-hidden');
+        $this.wrap('<div class="select"></div>');
+        $this.after('<div class="select-styled"></div>');
+
+        var $styledSelect = $this.next('div.select-styled');
+        $styledSelect.text($this.children('option').eq(0).text());
+
+        var $list = $('<ul />', {
+            'class': 'select-options'
+        }).insertAfter($styledSelect);
+
+        for (var i = 0; i < numberOfOptions; i++) {
+            $('<li />', {
+                text: $this.children('option').eq(i).text(),
+                rel: $this.children('option').eq(i).val()
+            }).appendTo($list);
+        }
+
+        var $listItems = $list.children('li');
+
+        $styledSelect.click(function(e) {
+            e.stopPropagation();
+            $('div.select-styled.active').not(this).each(function() {
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+
+        $listItems.click(function(e) {
+            e.stopPropagation();
+            $styledSelect.text($(this).text()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+            //console.log($this.val());
+        });
+
+        $(document).click(function() {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
+
+    });
+
+}
+
+
+
+function rangeSlider(elementRange, inputOne, inputTwo) {
+
+
+    var stepsSlider = document.querySelector(elementRange);
+    var input0 = document.querySelector(inputOne);
+    var input1 = document.querySelector(inputTwo);
+    var inputs = [input0, input1];
+
+    noUiSlider.create(stepsSlider, {
+        start: [40, 160],
+        connect: true,
+        range: {
+            'min': [0],
+            'max': 200
+        }
+    });
+
+    stepsSlider.noUiSlider.on('update', function(values, handle) {
+        inputs[handle].value = values[handle];
+    });
+
+
+    inputs.forEach(function(input, handle) {
+
+        input.addEventListener('change', function() {
+            stepsSlider.noUiSlider.setHandle(handle, this.value);
+        });
+
+        input.addEventListener('keydown', function(e) {
+
+            var values = stepsSlider.noUiSlider.get();
+            var value = Number(values[handle]);
+
+            // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+            var steps = stepsSlider.noUiSlider.steps();
+
+            // [down, up]
+            var step = steps[handle];
+
+            var position;
+
+
+        });
+    });
+}
 
 
 function yaMap() {
@@ -68,7 +170,14 @@ function files() {
 
 $(function() {
 
+    selectFun();
+
+    rangeSlider('.filter__range_square-js', '.filter__value_square-0-js', '.filter__value_square-1-js');
+
+    rangeSlider('.filter__range_price-js', '.filter__value_price-0-js', '.filter__value_price-1-js');
+
     yaMap();
+
     files();
 
     $('.promo__carousel').owlCarousel({
